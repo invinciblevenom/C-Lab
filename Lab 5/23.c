f2a6ed5fc1337bd9d51d371432ac4f93f5f8a782
Ansh Singh <anshsingh.14.lko@gmail.com>
2021-04-01 12:05:49 +0530

    Update Program 23

--- Lab 5/23.c
+++ Lab 5/23.c
@@ -1,3 +1,18 @@
 /*
 Write a program to convert decimal number in to binary number.
 */
+
+#include <stdio.h>
+
+long decToBin (long n) {
+	if (n == 0)
+		return 0;
+	return (n%2 + 10*decToBin(n/2));
+}
+
+int main(int argc, const char * argv[]) {
+	long n;
+	printf("This program will convert a decimal number into binary using recursion.\n\tEnter the decimal number : ");
+	scanf("%ld", &n);
+	printf("\n\t %ld|d = %ld|b\n\n", n, decToBin(n));
+}
