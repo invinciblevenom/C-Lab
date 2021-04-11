7edffb9502d913f43d57c24f4936bd141dba6ba5
Kshitij Kumar <40200770+gyarados025@users.noreply.github.com>
2021-04-11 16:03:34 +0530

    initial commit
    check in codeblocks

--- Lab 2/9.c
+++ Lab 2/9.c
@@ -1,3 +1,35 @@
 /*
 Write a program to generate sum of series 1!+2!+3!+--------------n!
 */
+
+#include "iostream"
+using namespace std;
+int factorial(int num){
+int i;
+int fact =1;
+i = num;
+while(num)
+{
+    fact *= num;
+    --num;
+
+}
+return fact;
+
+}
+
+int main()
+{
+    int n, sum = 0;
+
+    cout << "Enter the value of n : " << endl;
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        sum += fact(i);
+    }
+
+    cout << "Sum : " << sum << endl;
+}
