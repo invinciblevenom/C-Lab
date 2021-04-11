c957a835db4f5d6cfe0124aeb89098450185fe1d
Ansh Singh <anshsingh.14.lko@gmail.com>
2021-04-11 12:38:07 +0530

    Bug fix

--- Lab 5/23.c
+++ Lab 5/23.c
@@ -4,22 +4,24 @@ Write a program to convert decimal number in to binary number.
 
 #include <math.h>
 #include <stdio.h>
-int convert(long long n);
+long long convert(int n);
 int main() {
-    long long n;
-    printf("Enter a binary number: ");
-    scanf("%lld", &n);
-    printf("%lld in binary = %d in decimal", n, convert(n));
+    int n;
+    printf("Enter a decimal number: ");
+    scanf("%d", &n);
+    printf("%d in decimal = %lld in binary", n, convert(n));
     return 0;
 }
 
-int convert(long long n) {
-    int dec = 0, i = 0, rem;
+long long convert(int n) {
+    long long bin = 0;
+    int rem, i = 1, step = 1;
     while (n != 0) {
-        rem = n % 10;
-        n /= 10;
-        dec += rem * pow(2, i);
-        ++i;
+        rem = n % 2;
+        printf("Step %d: %d/2, Remainder = %d, Quotient = %d\n", step++, n, rem, n / 2);
+        n /= 2;
+        bin += rem * i;
+        i *= 10;
     }
-    return dec;
+    return bin;
 }
