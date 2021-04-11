cc62b59e983ccbb7e8d830075fee296b51f77eab
Kshitij Kumar <40200770+gyarados025@users.noreply.github.com>
2021-04-11 16:08:28 +0530

    Update 10.c
    incomplete program.

--- Lab 2/10.c
+++ Lab 2/10.c
@@ -1,3 +1,12 @@
 /*
 Write a program to find the sum of following series 1-X1/1!+X2/2!-…………Xn/n!
 */
+#include"iostream"
+#include"math"
+using namespace std;
+
+int main()
+{
+  
+  float x,t,sum;
+  int i,n=
