3add87e966bcc8ba1c0300231510ae652e52dcb1
Kshitij Kumar <40200770+gyarados025@users.noreply.github.com>
2021-04-11 15:54:34 +0530

    initial commit
    check using codeblocks

--- Lab 2/7.c
+++ Lab 2/7.c
@@ -1,3 +1,24 @@
 /*
 Write a program to construct a Fibonacci series upto n terms.
 */
+#include "iostream"
+using namespace std;
+
+int main() {
+
+  unsigned long x,y,z,n;
+  x=0;
+  y=1;
+  cout<<"Enter No of Elemets: "<<endl;
+  cin>>n;
+  cout<<x<<" "<<y;
+  for(int i = 2; i < n ; ++i)
+  {
+    z = x+y;
+    cout<<" "<<z;
+    x = y;
+    y = z;
+  } 
+  
+    return 0;
+}
