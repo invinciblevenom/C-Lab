04e2920f135d5d7acb46cb1166285b6bdf05abbe
Ansh Singh <anshsingh.14.lko@gmail.com>
2021-04-17 22:57:22 +0530

    Update 1.1: Drop use of getchar
    Simplify code and dropped use of getchar

--- Lab 1/1.c
+++ Lab 1/1.c
@@ -5,37 +5,22 @@ Write a program to calculate the area of triangle using formula at=√s(sa)(s-b)
 #include <math.h>
 #include <stdio.h>
 
-int main() {
-	int s, a = 0, b = 0, c = 0, ch = 0;
-	double area;
+int main() 
+{
+int s, a, b, c;
+double area;
 
-	do {
-		printf("Input the lenght of the first side of the triangle: ");
-		scanf("%i", &a);
-		// clean up the input buffer in case of bad characters
-		while((ch = getchar()) != '\n' && ch != EOF);
-	} while (a <= 0);
+printf("Input the length of the first side of the triangle: ");
+scanf("%i", &a);
+	
+printf("Input the length of the second side of the triangle: ");
+scanf("%i", &b);
 
-	do {
-		printf("Input the lenght of the second side of the triangle: ");
-		scanf("%i", &b);
-		while((ch = getchar()) != '\n' && ch != EOF);
-	} while (b <= 0);
-	do {
-		printf("Input the lenght of the third side of the triangle: ");
-		scanf("%i", &c);
-		while((ch = getchar()) != '\n' && ch != EOF);
-	} while (c <= 0);
+printf("Input the length of the third side of the triangle: ");
+scanf("%i", &c);
 
-	s = a + b + c;
-	s /= 2;
+s = (a + b + c) / 2;
+area = sqrt(s *(s - a) * (s - b) * (s - c));
 
-	area = sqrt(
-		s *
-		(s - a) *
-		(s - b) *
-		(s - c)
-	);
-
-	printf("The area of the triangle is %f \n", area);
+printf("The area of the triangle is %f \n", area);
 }
