df844e60c03024621047a31fc2ac747022df45b3
Ansh Singh <anshsingh.14.lko@gmail.com>
2021-04-17 23:05:31 +0530

    Update 1.3: Drop use of char and float
    Simplify code

--- Lab 1/3.c
+++ Lab 1/3.c
@@ -2,55 +2,33 @@
  Write a program to determine the roots of quadratic equation.
  */
  
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
-void getData(float *a, float *b, float *c) {
-	printf("\n\tEnter 'a': ");
-	scanf("%f", a);
-	printf("\tEnter 'b': ");
-	scanf("%f", b);
-	printf("\tEnter 'c': ");
-	scanf("%f", c);
-	printf("\n\n");
+int main() 
+{
+double a, b, c, d, root1, root2, realPart, imagPart;
+printf("Enter coefficients a, b and c: ");
+scanf("%lf %lf %lf", &a, &b, &c);
+
+d = b * b - 4 * a * c;
+
+if (d > 0) 
+{
+root1 = (-b + sqrt(d)) / (2 * a);
+root2 = (-b - sqrt(d)) / (2 * a);
+printf("root1 = %.2lf and root2 = %.2lf", root1, root2);
 }
-
-void print_roots(double d, double *x1, double *x2) {
-	if (d < 0) {
-		printf("\n\tImaginary Roots:\n\t\t%.4f + %.4fi\n\t\t%.4f - %.4fi\n\n", *x1, *x2, *x1, *x2);
-	}
-	else if (d > 0) {
-		printf("\n\tReal and Distinct Roots:\n\t\t%.4f\n\t\t%.4f", *x1, *x2);
-	}
-	else {
-		printf("\n\tReal and Equal Roots:\n\t\t%.4f\n\t\t%.4f", *x1, *x2);
-	}
-	printf("\n\n");
-}
-
-void quadratic(float a, float b, float c, double *x1, double *x2) {
-	double d = b*b - 4*a*c;
-	if (d < 0) {
-		*x1 = -b/(2*a);
-		*x2 = sqrt(fabs(d))/(2*a);
-		print_roots(d, x1, x2);
-	}
-	else if (d > 0) {
-		*x1 = (-b + sqrt(d))/(2*a);
-		*x2 = (-b - sqrt(d))/(2*a);
-		print_roots(d, x1, x2);
-	}
-	else {
-		*x1 = *x2 = -b/(2*a);
-		print_roots(d, x1, x2);
-	}
-}
-
-int main(int argc, const char *argv[]) {
-	float a, b, c;
-	printf("\n\tThis program will calculate roots of a quadratic equation.\n");
-	getData(&a, &b, &c);
-	double x1, x2;
-	quadratic(a, b, c, &x1, &x2);
-	return 0;
+else if (d == 0) {
+     root1 = root2 = -b / (2 * a);
+     printf("root1 = root2 = %.2lf;", root1);
+    }
+	
+    else 
+	{
+       realPart = -b / (2 * a);
+       imagPart = sqrt(-d) / (2 * a);
+       printf("root1 = %.2lf+%.2lfi and root2 = %.2f-%.2fi", realPart, imagPart, realPart, imagPart);
+    }
+return 0;
 }
