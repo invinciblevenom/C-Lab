1136208df7ae4295071e838e2132084f0c293655
Ansh Singh <anshsingh.14.lko@gmail.com>
2021-04-17 23:12:52 +0530

    Revert "Update 10.c"
    This reverts commit cc62b59e983ccbb7e8d830075fee296b51f77eab.

--- Lab 2/10.c
+++ Lab 2/10.c
@@ -1,12 +1,3 @@
 /*
 Write a program to find the sum of following series 1-X1/1!+X2/2!-…………Xn/n!
 */
-#include"iostream"
-#include"math"
-using namespace std;
-
-int main()
-{
-  
-  float x,t,sum;
-  int i,n=
